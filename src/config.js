acf9e405cb526fbdf800ae40622ce8a6e51a32af
QuanqiChen <1919334430@qq.com>
2024-10-07 16:29:02 +0800

    新建

---
+++ src/config.js
@@ -0,0 +1,120 @@
+export default {
+  // page1部分
+  page1: {
+    titleEn: 'Hello,I`m ChenQuan', //英文标题
+    title: '一个追梦在互联网路上的小白', //中文标题
+  },
+  // page2部分
+  page2: {
+    authorImg: 'chen.jpg', // 作者头像
+    xinhui: '我叫陈全，是一名在读的大四学生。', // 幸会
+    qiuzhi: '安卓开发，Java开发', // 求职意向
+    guanyuwo: '掌握了前端js、html、css基本技术和Vue框架、UI框架，nodejs等等，有着公司项目实战的经验和一些java安卓开发经历。希望可以和大家一起学习，共同进步！' // 关于我
+  },
+  // page3部分
+  page3: [{
+    icon: 'icon-tubiao-',
+    title: 'UI设计',
+    msg: ['PS切图、界面排版']
+  },{
+    icon: 'icon-diannao', // 图标
+    title: '网页制作', // 标题
+    msg: ['响应式页面', 'css、scss动画效果',] //介绍
+  }, {
+    icon: 'icon-qianbi1',
+    title: '前端功能',
+    msg: ['JS完成常见的交互功能', '用AJAX读取后台数据且渲染']
+  },  {
+    icon: 'icon-shouji',
+    title: '框架',
+    msg: ['SpringBoot框架','Android、mysql',]
+  }],
+  // page4部分
+  page4: {
+    // 我的历程
+    course: [{
+      date: '2024/7——至今', // 时间
+      desc: { // 经历
+        title: '酷赛集团',
+        list: ['安卓开发实习']
+      }
+    }, {
+      date: '2021/9-至今',
+      desc: {
+        title: '成都理工大学',
+        list: ['学习软件开发','国家励志奖学金','全国大学生博弈赛国家级一等奖','大唐杯省级二等奖','计算机设计大赛三等奖']}
+    }],
+    // 我的拓展技能掌握
+    singlelist: [{
+      title: 'Java',
+      text: '掌握'
+    }, {
+      title: 'SpringBoot',
+      text: '掌握'
+    }, {
+      title: 'Android开发',
+      text: '掌握'
+    }, {
+      title: '数据分析',
+      text: '了解'
+    }],
+    // 我的基本技能掌握
+    proresslist: [{
+      title: 'Java',
+      value: '90%'
+    }, {
+      title: 'Android',
+      value: '90%'
+    }, {
+      title: 'SpringBoot',
+      value: '90%'
+    }, {
+      title: 'SQL',
+      value: '80%'
+    }, {
+      title: '微服务',
+      value: '80%'
+    }]
+  },
+  // page5部分
+  page5: [{
+    title: '技术交易转化平台',
+    content: '技术交易转化平台',
+    image: 'box1.jpg',
+    href: 'https://gitee.com/quanqichen/technology-trading-platform'
+  }, {
+    title: '小小跑酷游戏服务端',
+    content: '小小跑酷游戏服务端，包括数据存储以及联机功能',
+    image: 'box3.png',
+    href: 'https://gitee.com/quanqichen/gbackend'
+  }, {
+    title: '小泉外卖',
+    content: '自研外卖平台',
+    image: 'box5.png',
+    href: 'https://gitee.com/quanqichen/koizumi-fried-rice-takeout'
+  }, {
+    title: '代码芝士',
+    content: '代码相关文章存储网站，个人博客网站',
+    image: 'box22.jpg',
+    href: 'https://gitee.com/quanqichen/codehome'
+  }, {
+    title: '宜宾四季乡愁博物馆',
+    content: '虚拟现实技术课程设计，宜宾四季乡愁博物馆',
+    image: 'box4.jpg',
+    href: 'https://gitee.com/quanqichen/bowuguan'
+  }, {
+    title: '中药大冒险',
+    content: '计算机设计大赛项目，游戏中药大冒险',
+    image: 'box6.jpg',
+    href: 'https://gitee.com/quanqichen/design-competition-zho'
+  }],
+  // page6部分
+  page6: {
+    github: 'https://gitee.com/quanqichen',
+    bili: '',
+    email: 'Mailto:1919334430@qq.com?Subject=邮箱标题&Body=邮箱内容！',
+    zhihu: '',
+    weixin: "<img style='width:120px' src='http://wttandroid.gitee.io/wttandroid.github.io/src/img/weix.jpg' alt='加载失败'>",
+    qq: "<img style='width:120px' src='http://wttandroid.gitee.io/wttandroid.github.io/src/img/qq.jpg' alt='加载失败'>"
+  }
+}
